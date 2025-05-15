06de0aaa9fe4cf0905b4929b897563df9cf53a5a
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-05-15 11:11:54 +0700

    Menambahkan method Traverse kepada double linked list

--- DoublyLinked-List.cpp
+++ DoublyLinked-List.cpp
@@ -114,6 +114,24 @@ public:
         delete current;
         cout << "Record with roll number " << rollNo << "Deleted" << endl;
     }
+
+    void traverse(){
+        if (START == NULL){
+            cout << "\nList is empty" << endl;
+            return;
+        }
+        //Step 1: Mark first node as currentNode
+        Node *currentNode = START;
+        //Step 2: Repeat until currentNode == NULL
+        cout << "\nRecords in ascending order of roll number are: \n";
+        int i = 0;
+        while (currentNode != NULL){
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+            //Step 3: Move to the next node
+            currentNode = currentNode->next;
+            i++;
+        }
+    }
 };
 
 int main (){
